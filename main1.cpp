93f3f1e098c45b05b3e8fe7428fdf73fbc82c60a
konesta1 <elhadjiissakone@gmail.com>
2019-12-05 22:17:31 +0100

    2ieme main

---
+++ main1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<conio.h>
+#include<string>
+#include<time.h>
+#include<stdlib.h>
+#include<windows.h>
+#include<Diams.h>
+#include<Plateau.h>
+
+
+using namespace std;
+
+int main()
+{
+  //Teupors teu(50,5);
+ // cout << teu.getTeuporsX()<< endl;;
+   // Diams d;
+    //d.afficher();
+//while(true){
+        //Teupors teupors(5,4);
+        Plateau plateau;
+
+        plateau.dessiner();
+
+       // Sleep(500);
+    //}
+
+
+
+   // cout << "Hello world!" << endl;
+    return 0;
+}
